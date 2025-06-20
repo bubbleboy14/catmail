4bc358de65692b1ec2288efd8771e69e88122ab4
bubbleboy14 <mario.balibrera@gmail.com>
2025-06-20 04:00:53 -0700

    vpush 0.1.2

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # catmail
 simple classes for email automation
 
-v0.1.1
+v0.1.2
--- catmail/__init__.py
+++ catmail/__init__.py
@@ -3,4 +3,4 @@ from .mailer import Mailer
 from .reader import Reader
 from .scanner import Scanner
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
